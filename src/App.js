c181f2b0f712a7700698557fd18e4b3bdb462db2
Amélie Chabrand <amelie.chabrand@gmail.com>
2022-11-21 15:06:54 +0100

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard';
+import Container from './components/Container';
 
 function App() {
   return (
@@ -18,6 +20,30 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Container>
+        <IdCard
+          lastName="Doe"
+          firstName="John"
+          gender="male"
+          height={178}
+          birth={new Intl.DateTimeFormat('fr-FR').format(
+            new Date('1992-07-14')
+          )}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+
+        <IdCard
+          lastName="Delores "
+          firstName="Obrien"
+          gender="female"
+          height={172}
+          birth={new Intl.DateTimeFormat('fr-FR').format(
+            new Date('1988-05-11')
+          )}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </Container>
     </div>
   );
 }
